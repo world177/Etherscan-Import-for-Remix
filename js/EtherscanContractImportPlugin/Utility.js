83d858de034e0e6ce7ce654c7cefad172d8c239c
Parker Andrew <parker@andrew.cx>
2022-04-17 01:51:20 -0500

    Fixes html format. Adds the used scripts for bootstrap to the project for if the links die.

--- js/EtherscanContractImportPlugin/Utility.js
+++ js/EtherscanContractImportPlugin/Utility.js
@@ -3,3 +3,20 @@ function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 
 }
+
+// a function to check if the page is in an iFrame
+//
+// https://stackoverflow.com/questions/326069/how-to-identify-if-a-webpage-is-being-loaded-inside-an-iframe-or-directly-into-t
+function inIframe () {
+
+    try {
+
+        return window.self !== window.top;
+
+    } catch (e) {
+
+        return true;
+
+    }
+
+}
